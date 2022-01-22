643eab4ed06b253bfb914a5634b58ae18b6ff751
Yash Kumar Verma <yk.verma2000@gmail.com>
2022-01-23 00:24:40 +0530

    Deploy website - based on 41714d8ec588f777c5df21da88abae40cc43d80a

--- .docusaurus/client-modules.js
+++
@@ -1,8 +0,0 @@
-export default [
-  require("/home/yash/Desktop/works/yashkumarverma.github.io/node_modules/remark-admonitions/styles/infima.css"),
-  require("/home/yash/Desktop/works/yashkumarverma.github.io/node_modules/remark-admonitions/styles/infima.css"),
-  require("/home/yash/Desktop/works/yashkumarverma.github.io/node_modules/remark-admonitions/styles/infima.css"),
-  require("/home/yash/Desktop/works/yashkumarverma.github.io/node_modules/infima/dist/css/default/default.css"),
-  require("/home/yash/Desktop/works/yashkumarverma.github.io/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/yash/Desktop/works/yashkumarverma.github.io/src/css/custom.css"),
-];
